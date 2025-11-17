de1ccc375fce96cdee48fe7e39c00a231875644c
Kyle Schmottlach <kyleschmottlach@gmail.com>
2025-11-17 14:45:01 -0500

    Started working on bare metal GPIO config

--- esp32/workspace/blink_manual/main/main.c
+++ esp32/workspace/blink_manual/main/main.c
@@ -10,15 +10,23 @@
 #include "hal/timer_types.h"
 #include "soc/clk_tree_defs.h"
 #include "soc/gpio_num.h"
+#include "soc/gpio_reg.h"
+#include "soc/io_mux_reg.h"
 
-#define BLINK_GPIO GPIO_NUM_0
+#define BLINK_GPIO GPIO_NUM_8
+
+uint32_t *gpio_func8_out_sel_cfg_reg = (uint32_t *) GPIO_FUNC8_OUT_SEL_CFG_REG;
+uint32_t *gpio_enable_w1ts_reg = (uint32_t *) GPIO_ENABLE_W1TS_REG;
+uint32_t *io_mux_gpio8_reg = (uint32_t *) IO_MUX_GPIO8_REG;
 
 // Function stubs
-void configure_led_gpio(void);
+void configure_led_gpio_hal(void);
+void configure_led_gpio_bare(void);
 void configure_timer(void);
 bool timer_alarm_callback(gptimer_handle_t timer, const gptimer_alarm_event_data_t *edata, void *user_ctx);
 
-void switch_led();
+void switch_led_hal();
+void switch_led_bare();
 
 // State variable storing LED state
 static uint32_t led_state = 0;
@@ -46,12 +54,24 @@ gptimer_event_callbacks_t const gptimer_cbs = {
 /**
 * Helper function to configure the GPIO pin for the output LED
 */
-void configure_led_gpio(void) {
+void configure_led_gpio_hal(void) {
 	gpio_reset_pin(BLINK_GPIO); // Resets the settings for the GPIO pin. Necessary since the boot process can change the pin settings
 										  // especially on special "strapping" pins
 	gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT); // Set the GPIO mode to output
 	gpio_set_pull_mode(BLINK_GPIO, GPIO_FLOATING); // Ensure that the pull mode is set to floating
 }
+
+void configure_led_gpio_bare(void) {
+	// Helper macros to disable input mode on the GPIO pin, and the pulldown and pullup resistors
+	PIN_INPUT_DISABLE(IO_MUX_GPIO8_REG);
+	PIN_PULLDWN_DIS(IO_MUX_GPIO8_REG);
+	PIN_PULLUP_DIS(IO_MUX_GPIO8_REG);
+
+	// Direct register setting of output mode via the GPIO_OUT_REG
+	*gpio_func8_out_sel_cfg_reg = *gpio_func8_out_sel_cfg_reg & (128 & GPIO_FUNC8_OUT_SEL);
+	*gpio_enable_w1ts_reg = *gpio_enable_w1ts_reg & (1 << BLINK_GPIO);
+}
+
 
 /**
 * Helper function to configure the settings for the general purpose timer and its alarm events
@@ -74,7 +94,8 @@ void configure_timer(void) {
 * - user_ctx: arbitrary context data that the user can pass in to the function. Can be anything
 */
 bool timer_alarm_callback(gptimer_handle_t timer, const gptimer_alarm_event_data_t *edata, void *user_ctx) {
-	switch_led(); // Switch the LED output every alarm
+	// switch_led_hal(); // Switch the LED output every alarm
+	switch_led_bare();
 
 	return false;
 }
@@ -82,16 +103,27 @@ bool timer_alarm_callback(gptimer_handle_t timer, const gptimer_alarm_event_data
 /**
 * Function that switches the LED state via the GPIO pins
 */
-void switch_led() {
+void switch_led_hal() {
 	ESP_ERROR_CHECK(gpio_set_level(BLINK_GPIO, led_state)); // Sets the level of the GPIO pin to either high or low
 
 	led_state = !led_state; // Flips the led state
 }
 
+void switch_led_bare() {
+	if(led_state) {
+		*((uint32_t *) GPIO_OUT_W1TS_REG) = *((uint32_t *) GPIO_OUT_W1TS_REG) & (1U << BLINK_GPIO);
+	} else {
+		*((uint32_t *) GPIO_OUT_W1TC_REG) = *((uint32_t *) GPIO_OUT_W1TC_REG) & (1U << BLINK_GPIO);
+	}
+
+	led_state = !led_state;
+}
+
 void app_main(void)
 {
 	// Configure the led and the timer
-	configure_led_gpio();
+	// configure_led_gpio_hal();
+	configure_led_gpio_bare();
 	configure_timer();
 
 	// Perform a loop 

9189516945ceb996f94fae3fb564840f9f713683
Kyle Schmottlach <kyleschmottlach@gmail.com>
2025-11-20 04:08:49 -0500

    Got LED's working, not buttons because of Watchdog timer

--- esp32/workspace/blink_manual/main/main.c
+++ esp32/workspace/blink_manual/main/main.c
@@ -6,18 +6,28 @@
 #include "driver/gptimer.h"
 #include "esp_err.h"
 #include "esp_log.h"
+#include "esp_task_wdt.h"
 #include "hal/gpio_types.h"
 #include "hal/timer_types.h"
+#include "rom/rtc.h"
 #include "soc/clk_tree_defs.h"
 #include "soc/gpio_num.h"
 #include "soc/gpio_reg.h"
 #include "soc/io_mux_reg.h"
+#include "rtc_wdt.h"
 
-#define BLINK_GPIO GPIO_NUM_8
+#define REG(REGISTER) ((volatile uint32_t *) (REGISTER))
 
-uint32_t *gpio_func8_out_sel_cfg_reg = (uint32_t *) GPIO_FUNC8_OUT_SEL_CFG_REG;
-uint32_t *gpio_enable_w1ts_reg = (uint32_t *) GPIO_ENABLE_W1TS_REG;
-uint32_t *io_mux_gpio8_reg = (uint32_t *) IO_MUX_GPIO8_REG;
+#define BLUE_LED GPIO_NUM_8
+#define RED_LED GPIO_NUM_1
+#define YELLOW_LED GPIO_NUM_0
+#define GREEN_LED GPIO_NUM_7
+
+#define BLUE_BUTTON GPIO_NUM_10
+#define RED_BUTTON GPIO_NUM_11
+#define YELLOW_BUTTON GPIO_NUM_2
+#define GREEN_BUTTON GPIO_NUM_3
+
 
 // Function stubs
 void configure_led_gpio_hal(void);
@@ -25,8 +35,14 @@ void configure_led_gpio_bare(void);
 void configure_timer(void);
 bool timer_alarm_callback(gptimer_handle_t timer, const gptimer_alarm_event_data_t *edata, void *user_ctx);
 
+void reset_gpio_bare(uint8_t gpio_pin);
+void set_gpio_output(uint8_t gpio_pin);
+void set_gpio_input(uint8_t gpio_pin, bool pullup);
+
 void switch_led_hal();
 void switch_led_bare();
+
+uint32_t get_io_mux_from_gpio(uint8_t gpio_pin);
 
 // State variable storing LED state
 static uint32_t led_state = 0;
@@ -51,25 +67,98 @@ gptimer_event_callbacks_t const gptimer_cbs = {
 	.on_alarm = timer_alarm_callback // Sets the callback for the alarm event
 };
 
+uint32_t get_io_mux_from_gpio(uint8_t gpio_pin) {
+	switch(gpio_pin){
+	case BLUE_LED:
+		return IO_MUX_GPIO8_REG;
+	case RED_LED:
+		return IO_MUX_GPIO1_REG;
+	case YELLOW_LED:
+		return IO_MUX_GPIO0_REG;
+	case GREEN_LED:
+		return IO_MUX_GPIO7_REG;
+	case BLUE_BUTTON:
+		return IO_MUX_GPIO10_REG;
+	case RED_BUTTON:
+		return IO_MUX_GPIO11_REG;
+	case YELLOW_BUTTON:
+		return IO_MUX_GPIO2_REG;
+	case GREEN_BUTTON:
+		return IO_MUX_GPIO3_REG;
+	default:
+		return 0;
+	}
+}
+
 /**
 * Helper function to configure the GPIO pin for the output LED
 */
 void configure_led_gpio_hal(void) {
-	gpio_reset_pin(BLINK_GPIO); // Resets the settings for the GPIO pin. Necessary since the boot process can change the pin settings
+	gpio_reset_pin(BLUE_LED); // Resets the settings for the GPIO pin. Necessary since the boot process can change the pin settings
 										  // especially on special "strapping" pins
-	gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT); // Set the GPIO mode to output
-	gpio_set_pull_mode(BLINK_GPIO, GPIO_FLOATING); // Ensure that the pull mode is set to floating
+	gpio_set_direction(BLUE_LED, GPIO_MODE_OUTPUT); // Set the GPIO mode to output
+	gpio_set_pull_mode(BLUE_LED, GPIO_FLOATING); // Ensure that the pull mode is set to floating
+}
+
+void reset_gpio_bare(uint8_t gpio_pin) {
+	uint32_t io_mux_register = get_io_mux_from_gpio(gpio_pin);	
+
+	PIN_INPUT_DISABLE(io_mux_register);
+	PIN_PULLDWN_DIS(io_mux_register);
+	PIN_PULLUP_DIS(io_mux_register);
+}
+
+void set_gpio_output(uint8_t gpio_pin) {
+	PIN_FUNC_SELECT(get_io_mux_from_gpio(gpio_pin), FUNC_GPIO8_GPIO8);
+
+	*REG(GPIO_FUNC0_OUT_SEL_CFG_REG + 4*gpio_pin) |= 128 & GPIO_FUNC0_OUT_SEL;
+	*REG(GPIO_FUNC0_OUT_SEL_CFG_REG + 4*gpio_pin) |= GPIO_FUNC0_OEN_SEL;
+	*REG(GPIO_ENABLE_W1TS_REG) = (1 << gpio_pin);
+}
+
+void set_gpio_input(uint8_t gpio_pin, bool pullup) {
+	*REG(GPIO_FUNC0_IN_SEL_CFG_REG + 4*gpio_pin) |= GPIO_SIG0_IN_SEL;
+	*REG(GPIO_FUNC0_IN_SEL_CFG_REG + 4*gpio_pin) |= (1U << gpio_pin) & GPIO_FUNC0_IN_SEL;
+
+	uint32_t io_mux_reg = get_io_mux_from_gpio(gpio_pin);
+
+	PIN_INPUT_ENABLE(io_mux_reg);
+
+	if(pullup) {
+		PIN_PULLUP_EN(io_mux_reg);
+		PIN_PULLDWN_DIS(io_mux_reg);
+	} else {
+		PIN_PULLDWN_EN(io_mux_reg);
+		PIN_PULLUP_DIS(io_mux_reg);
+	}
 }
 
 void configure_led_gpio_bare(void) {
 	// Helper macros to disable input mode on the GPIO pin, and the pulldown and pullup resistors
-	PIN_INPUT_DISABLE(IO_MUX_GPIO8_REG);
-	PIN_PULLDWN_DIS(IO_MUX_GPIO8_REG);
-	PIN_PULLUP_DIS(IO_MUX_GPIO8_REG);
+	reset_gpio_bare(BLUE_LED);
+	reset_gpio_bare(RED_LED);
+	reset_gpio_bare(YELLOW_LED);
+	reset_gpio_bare(GREEN_LED);
+	reset_gpio_bare(BLUE_BUTTON);
+	reset_gpio_bare(RED_BUTTON);
+	reset_gpio_bare(YELLOW_BUTTON);
+	reset_gpio_bare(GREEN_BUTTON);
+	
 
 	// Direct register setting of output mode via the GPIO_OUT_REG
-	*gpio_func8_out_sel_cfg_reg = *gpio_func8_out_sel_cfg_reg & (128 & GPIO_FUNC8_OUT_SEL);
-	*gpio_enable_w1ts_reg = *gpio_enable_w1ts_reg & (1 << BLINK_GPIO);
+	// *gpio_func8_out_sel_cfg_reg |= 128 & GPIO_FUNC8_OUT_SEL;
+	// *gpio_func8_out_sel_cfg_reg |= GPIO_FUNC8_OEN_SEL;
+	// *gpio_enable_w1ts_reg = (1 << BLUE_LED);
+
+	set_gpio_output(BLUE_LED);
+	set_gpio_output(RED_LED);
+	set_gpio_output(YELLOW_LED);
+	set_gpio_output(GREEN_LED);
+
+	set_gpio_input(BLUE_BUTTON, true);
+	set_gpio_input(RED_BUTTON, true);
+	set_gpio_input(YELLOW_BUTTON, true);
+	set_gpio_input(GREEN_BUTTON, true);
 }
 
 
@@ -104,16 +193,16 @@ bool timer_alarm_callback(gptimer_handle_t timer, const gptimer_alarm_event_data
 * Function that switches the LED state via the GPIO pins
 */
 void switch_led_hal() {
-	ESP_ERROR_CHECK(gpio_set_level(BLINK_GPIO, led_state)); // Sets the level of the GPIO pin to either high or low
+	ESP_ERROR_CHECK(gpio_set_level(BLUE_LED, led_state)); // Sets the level of the GPIO pin to either high or low
 
 	led_state = !led_state; // Flips the led state
 }
 
 void switch_led_bare() {
 	if(led_state) {
-		*((uint32_t *) GPIO_OUT_W1TS_REG) = *((uint32_t *) GPIO_OUT_W1TS_REG) & (1U << BLINK_GPIO);
+		*REG(GPIO_OUT_W1TS_REG) |= (1U << BLUE_LED) | (1U << RED_LED) | (1U << YELLOW_LED) | (1U << GREEN_LED);
 	} else {
-		*((uint32_t *) GPIO_OUT_W1TC_REG) = *((uint32_t *) GPIO_OUT_W1TC_REG) & (1U << BLINK_GPIO);
+		*REG(GPIO_OUT_W1TC_REG)  |= (1U << BLUE_LED) | (1U << RED_LED) | (1U << YELLOW_LED) | (1U << GREEN_LED);
 	}
 
 	led_state = !led_state;
@@ -126,9 +215,39 @@ void app_main(void)
 	configure_led_gpio_bare();
 	configure_timer();
 
+	uint8_t lastBlueState = 0;
+	uint8_t lastRedState = 0;
+	uint8_t lastYellowState = 0;
+	uint8_t lastGreenState = 0;
+
 	// Perform a loop 
     while (true) {
-        sleep(UINT32_MAX); // Sleep the core for the maximum amount of time possible. Alarm events mean the CPU essentially does
+        //sleep(UINT32_MAX); // Sleep the core for the maximum amount of time possible. Alarm events mean the CPU essentially does
 									// no work on its own once they are running
-    }
+		static uint32_t count = 0;
+
+		uint32_t input = *REG(GPIO_IN_REG);
+		
+		// if(count % 1000 == 0) printf("Input Register: %u\n", (unsigned int)input);
+
+		uint8_t blueVal = ((input >> BLUE_BUTTON) & 1U);
+		uint8_t redVal = ((input >> RED_BUTTON) & 1U);
+		uint8_t yellowVal = ((input >> YELLOW_BUTTON) & 1U);
+		uint8_t greenVal = ((input >> GREEN_BUTTON) & 1U);
+
+		if(!blueVal && blueVal != lastBlueState) printf("Blue button pressed!");
+		if(!redVal && redVal != lastRedState) printf("Red button pressed!");
+		if(!yellowVal && yellowVal != lastYellowState) printf("Yellow button pressed!");
+		if(!greenVal && greenVal != lastGreenState) printf("Green button pressed!");
+
+		lastBlueState = blueVal;
+		lastRedState = redVal;
+		lastYellowState = yellowVal;
+		lastGreenState = greenVal;
+
+		count++;
+
+		*REG(SUPER_WDT_RESET);
+		usleep(1000);
+	}
 }

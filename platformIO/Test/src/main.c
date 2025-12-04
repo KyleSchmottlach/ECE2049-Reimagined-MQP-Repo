8fcb216e7bd13024a0505b4d2d9f456e8e6a4144
Kyle Schmottlach <kyleschmottlach@gmail.com>
2025-12-04 10:57:39 -0500

    Created project with PlatformIO

---
+++ platformIO/Test/src/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <time.h>
+#include <unistd.h>
+
+void app_main() {
+    
+    while (true) {
+        printf("Hello, world!\n");
+        sleep(1);
+    }
+}
